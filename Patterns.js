905da47609042ae7a19b7442b65d6011e078bb3d
Ravi Rathore <ravirathore15@icloud.com>
2020-05-17 15:13:09 +1000

    added decription for pattersn

---
+++ Patterns.js
@@ -0,0 +1,19 @@
+// Quick 5
+// Top line
+// Middle line 
+// Bottom line
+// Box bonus(each line mey 2 no. Katna chahiye)
+// Sheet bonus( 3 continuous ticket mey 2 no. Each katna hai...for e.g. ticket no 1,2,3 agar mey leta hu toh har ticket meh 2 no katna hai)
+// And 
+// Last 3 full house
+//  I.e first full house, second full house and third full house
+// [7:53 PM, 5/13/2020] Ravi Latrobe: One more prize: star ( corner nos of the ticket and middle no of middle line)
+// [7:53 PM, 5/13/2020] Ravi Latrobe: In this ticket for star: 2,80,55,8,79
+
+const ticket = 
+[[{ "num": 2, "marked": false }, { "num": 13, "marked": false }, { "num": 0, "marked": false }, { "num": 33, "marked": false }, { "num": 0, "marked": false }, { "num": 60, "marked": false }, { "num": 0, "marked": false }, { "num": 72, "marked": false }, { "num": 0, "marked": false }],
+[{ "num": 0, "marked": false }, { "num": 20, "marked": false }, { "num": 23, "marked": false }, { "num": 0, "marked": false }, { "num": 0, "marked": false }, { "num": 0, "marked": false }, { "num": 67, "marked": false }, { "num": 78, "marked": false }, { "num": 81, "marked": false }],
+[{ "num": 4, "marked": false }, { "num": 0, "marked": false }, { "num": 25, "marked": false }, { "num": 0, "marked": false }, { "num": 49, "marked": false }, { "num": 0, "marked": false }, { "num": 68, "marked": false }, { "num": 0, "marked": false }, { "num": 85, "marked": false }]]
+function isTopLine(ticket) {
+
+}
